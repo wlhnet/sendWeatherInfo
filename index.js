d40742380d8fcc895047444b41efd075e87f5e99
王同学 <shimily.xy@qq.com>
2022-08-23 02:02:04 +0800

    Update index.js

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ const app_secret = config.APP_SECRET
 const user_id = config.USER_ID
 const template_id = config.TEMPLATE_ID
 
-console.log(this)
+console.log(process.env)
 
 // 提取天气信息
 const {weather, temperature, temperature_low, temperature_high} = getWeather(city)
