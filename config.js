dd51f510881c2a2d3938568c998f88de233fc4ff
王同学 <shimily.xy@qq.com>
2022-08-23 01:19:46 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,12 +1,12 @@
 module.exports = {
-    APP_ID: "String",
-    APP_SECRET: "String",
-    CITY: "String", // 地级市（如："长沙"）
-    START_DATE: "xxxx-xx-xx",
-    BIRTHDAY1: "xx-xx",
-    BIRTHDAY2: "xx-xx",
-    USER_ID: "String",
-    TEMPLATE_ID: "String",
+    APP_ID: "wx19338547890d2293",
+    APP_SECRET: "fe3db78218ea30b029945052bbd61c15",
+    CITY: "北京",
+    START_DATE: "2022-07-01",
+    BIRTHDAY1: "10-05",
+    BIRTHDAY2: "11-01",
+    USER_ID: "oOOM16O-tBr9sZmUWXGdyzTR_Yec",
+    TEMPLATE_ID: "CNKmWX1iQYh1A1xxS7mLctqeBIv5fM50zxgecVDs5jA",
 }
 
 /* 数据源格式说明
