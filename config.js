6a0841dfcb488a186895e068955cb7dbe00bb218
王同学 <shimily.xy@qq.com>
2022-08-23 00:58:07 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 module.exports = {
     APP_ID: "String",
     APP_SECRET: "String",
-    CITY: "String", // 地级市（如：长沙）
+    CITY: "String", // 地级市（如："长沙"）
     START_DATE: "xxxx-xx-xx",
     BIRTHDAY1: "xx-xx",
     BIRTHDAY2: "xx-xx",
@@ -13,7 +13,7 @@ module.exports = {
 {
     APP_ID: "String",
     APP_SECRET: "String",
-    CITY: "String", // 地级市（如：长沙）
+    CITY: "String", // 地级市（如："长沙"）
     START_DATE: "xxxx-xx-xx",
     BIRTHDAY1: "xx-xx",
     BIRTHDAY2: "xx-xx",
